cb3130237d7530a14c9687ac19c45cd523833697
SlavaVasilyevQA <thenovalive@gmail.com>
2025-05-15 19:42:21 +0300

    Добавил в тест "test_create_booking.py" код из урока и добавил 1 негативный кейс, но он придуманный, жалко, что в документации нет различных статус кодов ответов

--- test/test_create_booking.py
+++ test/test_create_booking.py
@@ -1,11 +1,14 @@
 import allure
 from jsonschema import validate
 from core.settings.contracts import CREATE_BOOKING_DATA_SCHEME
+from pydantic import ValidationError
+from core.model.booking import BookingResponse
+import json
 
 
 @allure.feature("Проверка создания брони")
-@allure.story("Проверка валидного создания брони")
-def test_create_booking_valid(api_client, generate_random_booking_data):
+@allure.story("Позитивный кейс: Проверка создания брони с фэйковыми данными")
+def test_create_booking_with_fake_data(api_client, generate_random_booking_data):
     payload = generate_random_booking_data
     response = api_client.create_booking(payload)
     response_json = response.json()
@@ -27,3 +30,63 @@ def test_create_booking_valid(api_client, generate_random_booking_data):
         assert response_json["booking"]["bookingdates"]["checkout"] == payload["bookingdates"]["checkout"]
     with allure.step("Проверка поля \"additionalneeds\""):
         assert response_json["booking"]["additionalneeds"] == payload["additionalneeds"]
+
+
+@allure.feature("Проверка создания брони")
+@allure.story("Позитивный кейс: Проверка создания брони с кастомными данными")
+def test_create_booking_with_custom_data(api_client):
+    payload = {
+        "firstname": "Ivan",
+        "lastname": "Ivanovich",
+        "totalprice": 150,
+        "depositpaid": True,
+        "bookingdates": {
+            "checkin": "2025-05-10",
+            "checkout": "2025-05-25"
+        },
+        "additionalneeds": "Dinner"
+    }
+    response = api_client.create_booking(payload)
+    response_json = response.json()
+    try:
+        BookingResponse(**response_json)
+    except ValidationError as e:
+        raise ValidationError(f"Валидация провалилась: {e}")
+    with allure.step("Проверка поля \"firstname\""):
+        assert response_json["booking"]["firstname"] == payload["firstname"]
+    with allure.step("Проверка поля \"lastname\""):
+        assert response_json["booking"]["lastname"] == payload["lastname"]
+    with allure.step("Проверка поля \"totalprice\""):
+        assert response_json["booking"]["totalprice"] == payload["totalprice"]
+    with allure.step("Проверка поля \"depositpaid\""):
+        assert response_json["booking"]["depositpaid"] == payload["depositpaid"]
+    with allure.step("Проверка поля \"checkin\""):
+        assert response_json["booking"]["bookingdates"]["checkin"] == payload["bookingdates"]["checkin"]
+    with allure.step("Проверка поля \"checkout\""):
+        assert response_json["booking"]["bookingdates"]["checkout"] == payload["bookingdates"]["checkout"]
+    with allure.step("Проверка поля \"additionalneeds\""):
+        assert response_json["booking"]["additionalneeds"] == payload["additionalneeds"]
+
+
+@allure.feature("Проверка создания брони")
+@allure.story("Негативный кейс: Проверка создания брони без поля lastname")
+def test_create_booking_without_lastname(api_client):
+    payload = {
+        "firstname": "Ivan",
+        "totalprice": 150,
+        "depositpaid": True,
+        "bookingdates": {
+            "checkin": "2025-05-10",
+            "checkout": "2025-05-25"
+        },
+        "additionalneeds": "Dinner"
+    }
+    response_error = {
+        "error": "Отсутствует обязательное поле"
+    }
+    with allure.step("Отправка запроса без поля lastname"):
+        response = api_client.create_booking(payload)
+    with allure.step("Проверка статус кода"):
+        assert response.status_code == 400, f"Ожидали статус код 400 Bad Request, но получили {response.status_code} Bad Request"
+    with allure.step("Проверка наличия сообщения об ошибке"):
+        assert json.loads(response.text) == response_error
