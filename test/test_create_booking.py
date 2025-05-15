17727ded1014a182218a8186a99662715ed608b7
SlavaVasilyevQA <thenovalive@gmail.com>
2025-05-15 11:18:08 +0300

    Добавил проверки на соответствия payload и response.json()

--- test/test_create_booking.py
+++ test/test_create_booking.py
@@ -13,3 +13,17 @@ def test_create_booking_valid(api_client, generate_random_booking_data):
         assert response.status_code == 200, f"Ожидали статус код 200, но получили {response.status_code}"
     with allure.step("Валидация JSON-схемы"):
         validate(response_json, CREATE_BOOKING_DATA_SCHEME)
+    with allure.step("Проверка поля \"firstname\""):
+        assert response_json["booking"]["firstname"] == payload["firstname"]
+    with allure.step("Проверка поля \"lastname\""):
+        assert response_json["booking"]["lastname"] == payload["lastname"]
+    with allure.step("Проверка поля \"totalprice\""):
+        assert response_json["booking"]["totalprice"] == payload["totalprice"]
+    with allure.step("Проверка поля \"depositpaid\""):
+        assert response_json["booking"]["depositpaid"] == payload["depositpaid"]
+    with allure.step("Проверка поля \"checkin\""):
+        assert response_json["booking"]["bookingdates"]["checkin"] == payload["bookingdates"]["checkin"]
+    with allure.step("Проверка поля \"checkout\""):
+        assert response_json["booking"]["bookingdates"]["checkout"] == payload["bookingdates"]["checkout"]
+    with allure.step("Проверка поля \"additionalneeds\""):
+        assert response_json["booking"]["additionalneeds"] == payload["additionalneeds"]
